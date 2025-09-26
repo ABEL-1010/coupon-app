1e21f3aee82e9b36cd542d514a89713ff7fbf6c4
ABEL-1010 <abeldemoz4@gmail.com>
2025-09-26 16:54:10 +0300

    cors changed

--- server/server.js
+++ server/server.js
@@ -13,7 +13,17 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://coupon-app-front.onrender.com',
+    'http://localhost:3000',
+    'http://localhost:5173'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token']
+}));
+
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
